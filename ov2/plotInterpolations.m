83798376047b6724113d118209e42763432a6858
Simen Norderud jensen <simennj@outlook.com>
2017-09-25 21:24:09 +0200

    add plotting of interpolated motor and fan values

---
+++ ov2/plotInterpolations.m
@@ -0,0 +1,26 @@
+clc
+clear all
+close all
+
+motor=[0, 400, 800, 1200, 1600, 1763;
+       8, 8.5, 9.0, 9.90, 7.15,    0];
+
+fan=[0, 400, 800, 1200;
+     2, 3.8, 7.5,   14];
+
+figure(1)
+hold on
+[ motorSteps,motorLinear,motorNearest,motorPchip,motorSpline ] = interpolations(motor);
+plot(motor(1,:), motor(2,:), 'b+')
+plot(motorSteps, motorLinear, 'b-')
+plot(motorSteps, motorNearest, 'b--')
+plot(motorSteps, motorPchip, 'b:')
+plot(motorSteps, motorSpline, 'b-.')
+[ fanSteps,fanLinear,fanNearest,fanPchip,fanSpline ] = interpolations(fan);
+plot(fan(1,:), fan(2,:), 'r+')
+plot(fanSteps, fanLinear, 'r-')
+plot(fanSteps, fanNearest, 'r--')
+plot(fanSteps, fanPchip, 'r:')
+plot(fanSteps, fanSpline, 'r-.')
+legend('motor','linear', 'nearest','pchip','cubic','fan','linear', 'nearest','pchip','cubic')
+hold off
