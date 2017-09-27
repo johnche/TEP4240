03a8aec6bb7548642e1eeb973ecb8331457f2748
Simen <Simennj@outlook.com>
2017-09-27 12:40:24 +0200

    find max power and corresponding speed for motor

--- ov2/plotInterpolations.m
+++ ov2/plotInterpolations.m
@@ -9,6 +9,9 @@ fan=[0, 400, 800, 1200;
      2, 3.8, 7.5,   14];
 
 figure(1)
+
+ylabel('torque, in.-lb')
+xlabel('speed, rpm')
 hold on
 [ motorSpeeds,motorLinear,motorNearest,motorPchip,motorSpline ] = interpolations(motor);
 plot(motor(1,:), motor(2,:), 'b+')
@@ -16,15 +19,16 @@ plot(motor(1,:), motor(2,:), 'b+')
 %plot(motorSteps, motorNearest, 'b--')
 %plot(motorSteps, motorPchip, 'b:')
 plot(motorSpeeds, motorSpline, 'b-')
+[motorMaxPower, motorMaxPowerIndex] = max(motorSpline)
+motorMaxPower_speed = motorSpeeds(motorMaxPowerIndex)
+plot(motorMaxPower_speed, motorMaxPower, 'bd')
 [ fanSpeeds,fanLinear,fanNearest,fanPchip,fanSpline ] = interpolations(fan);
 plot(fan(1,:), fan(2,:), 'r+')
 %plot(fanSteps, fanLinear, 'r-.')
 %plot(fanSteps, fanNearest, 'r--')
 %plot(fanSteps, fanPchip, 'r:')
 plot(fanSpeeds, fanSpline, 'r-')
-legend('motor',...'linear','nearest','pchip',
-    'cubic','fan',...'linear','nearest','pchip',
-    'cubic')
-ylabel('torque, in.-lb')
-xlabel('speed, rpm')
+legend('motor real',...'linear','nearest','pchip',
+    'motor interpolated','motor max power', 'fan real',...'linear','nearest','pchip',
+    'fan interpolated','Location', 'northwest')
 hold off
