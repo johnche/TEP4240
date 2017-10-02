05f42ea8a7778048e823fdddec3d34072713113b
Simen <Simennj@outlook.com>
2017-10-02 21:26:40 +0200

    clean up dead code

--- ov2/interpolations.m
+++
@@ -1,6 +0,0 @@
-function [ linear,nearest,pchip,spline ] = interpolations( values, steps )
-    linear = interp1(values(1,:), values(2,:), steps);
-    nearest = interp1(values(1,:), values(2,:), steps, 'nearest');
-    pchip = interp1(values(1,:), values(2,:), steps, 'pchip');
-    spline = interp1(values(1,:), values(2,:), steps, 'spline');
-end
--- ov2/plotInterpolations.m
+++ ov2/plotInterpolations.m
@@ -16,8 +16,8 @@ hold on
 
 stepcount = 1000;
 speeds = linspace(motor(1), motor(1, end), stepcount);
-[ motorLinear,motorNearest,motorPchip,motorSpline ] = interpolations(motor, speeds);
-[ fanLinear,fanNearest,fanPchip,fanSpline ] = interpolations(fan, speeds);
+motorSpline = interp1(motor(1,:), motor(2,:), speeds, 'spline');
+fanSpline = interp1(fan(1,:), fan(2,:), speeds, 'spline');
 
 [maxPower,maxPower_motorIndex] = max(speeds.*motorSpline.*(motorSpline <= 4));
 [~,maxPower_fanIndex] = min(abs(speeds.*fanSpline-maxPower));
@@ -28,16 +28,10 @@ maxPower_fanSpeed = speeds(maxPower_fanIndex);
 beltRatio = maxPower_motorSpeed/maxPower_fanSpeed;
 
 plot(motor(1,:), motor(2,:), 'b+')
-%plot(motorSteps, motorLinear, 'b-.')
-%plot(motorSteps, motorNearest, 'b--')
-%plot(motorSteps, motorPchip, 'b:')
 plot(speeds, motorSpline, 'b-')
 plot(speeds(maxPower_motorIndex), motorSpline(maxPower_motorIndex), 'bd')
 
 plot(fan(1,:), fan(2,:), 'r+')
-%plot(fanSteps, fanLinear, 'r-.')
-%plot(fanSteps, fanNearest, 'r--')
-%plot(fanSteps, fanPchip, 'r:')
 plot(speeds, fanSpline, 'r-')
 plot(maxPower_fanSpeed, maxPower_fanTorque, 'rd')
 
