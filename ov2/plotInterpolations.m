617caf1e47cdf7be8e250f8ff61dfa9a18f28eb6
Simen <Simennj@outlook.com>
2017-10-02 21:09:37 +0200

    fix wrong max values

--- ov2/plotInterpolations.m
+++ ov2/plotInterpolations.m
@@ -13,36 +13,33 @@ figure(1)
 ylabel('torque, in.-lb')
 xlabel('speed, rpm')
 hold on
+
 stepcount = 1000;
-steps = linspace(motor(1), motor(1, end), stepcount);
-[ motorLinear,motorNearest,motorPchip,motorSpline ] = interpolations(motor, steps);
+speeds = linspace(motor(1), motor(1, end), stepcount);
+[ motorLinear,motorNearest,motorPchip,motorSpline ] = interpolations(motor, speeds);
+[ fanLinear,fanNearest,fanPchip,fanSpline ] = interpolations(fan, speeds);
+
+[maxMotorPower,maxPower_motorIndex] = max(speeds.*motorSpline.*(motorSpline <= 4));
+[~,maxPower_fanIndex] = min(abs(speeds.*fanSpline-maxMotorPower));
+
 plot(motor(1,:), motor(2,:), 'b+')
 %plot(motorSteps, motorLinear, 'b-.')
 %plot(motorSteps, motorNearest, 'b--')
 %plot(motorSteps, motorPchip, 'b:')
-plot(steps, motorSpline, 'b-')
-plot(steps, 4.*ones(stepcount))
+plot(speeds, motorSpline, 'b-')
+plot(speeds(maxPower_motorIndex), motorSpline(maxPower_motorIndex), 'bd')
 
-intersectionStep = 1;
-minIntersectionDiff = 99999;
-for k = 1:stepcount
-    difference = abs(motorSpline(k) - 4);
-    if difference < minIntersectionDiff
-        minIntersectionDiff = difference;
-        intersectionStep = k;
-    end
-end
-maxPower_RPM = steps(intersectionStep)
-plot(steps(intersectionStep), 4, 'X')
-[ fanLinear,fanNearest,fanPchip,fanSpline ] = interpolations(fan, steps);
 plot(fan(1,:), fan(2,:), 'r+')
 %plot(fanSteps, fanLinear, 'r-.')
 %plot(fanSteps, fanNearest, 'r--')
 %plot(fanSteps, fanPchip, 'r:')
-plot(steps, fanSpline, 'r-')
-maxPower_fanTorque = fanSpline(intersectionStep)
-plot(steps(intersectionStep), maxPower_fanTorque, 'X')
+plot(speeds, fanSpline, 'r-')
+maxPower_fanTorque = fanSpline(maxPower_fanIndex);
+maxPower_fanSpeed = speeds(maxPower_fanIndex);
+plot(maxPower_fanSpeed, maxPower_fanTorque, 'rd')
+
 legend('motor real',...'linear','nearest','pchip',
     'motor interpolated','motor max power', 'fan real',...'linear','nearest','pchip',
-    'fan interpolated','Location', 'northwest')
+    'fan interpolated','fan max power',...
+    'Location', 'northwest')
 hold off
