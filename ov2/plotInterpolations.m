9f4747e543c89f741e36ae849cce8ae185ade68a
Odd Harald Sjursen Sande <oddh123@gmail.com>
2017-10-05 20:27:26 +0200

    Add Simualtion of start-up to steady-state speed

--- ov2/plotInterpolations.m
+++ ov2/plotInterpolations.m
@@ -11,8 +11,8 @@ fan=[0, 400, 800, 1200;
 
 stepcount = 1000;
 speeds = linspace(motor(1), motor(1, end), stepcount);
-motorSpline = interp1(motor(1,:), motor(2,:), speeds, 'spline');
-fanSpline = interp1(fan(1,:), fan(2,:), speeds, 'spline');
+motorSpline = interp1(motor(1,:), motor(2,:), speeds, 'pchip');
+fanSpline = interp1(fan(1,:), fan(2,:), speeds, 'pchip');
 
 [maxPower,maxPower_motorIndex] = max(speeds.*motorSpline.*(motorSpline <= 4));
 [~,maxPower_fanIndex] = min(abs(speeds.*fanSpline-maxPower));
@@ -23,7 +23,7 @@ maxPower_fanSpeed = speeds(maxPower_fanIndex);
 beltRatio = maxPower_motorTorque/maxPower_fanTorque;
 
 fprintf('a)\n')
-fprintf('  S --> T --> R\n')
+fprintf('  S --> T --> L\n')
 
 fprintf('b)\n')
 fprintf('  max power = %f\n', maxPower)
@@ -56,3 +56,40 @@ legend('motor real',...'linear','nearest','pchip',
     'fan interpolated','fan max power','fan transformed',...
     'Location', 'northwest')
 hold off
+
+%e)
+tmax=30;
+dt=.1;                                        
+k=0;
+teta_dot=0;
+tvec = 0:dt:tmax;
+teta_dots_rpm = zeros(1, length(tvec));
+Im = 0.2;
+Iv = 1.2;
+T = beltRatio;
+for t= tvec
+    teta_rpm = teta_dot*60/(2*pi);
+    motorTorque = interp1(speeds, motorSpline, teta_rpm);
+    fanTorque = interp1(speeds/beltRatio, fanSpline*beltRatio, teta_rpm);
+    power_dot = motorTorque - fanTorque;
+    teta_ddot = power_dot/(Im + T^2*Iv);
+    teta_dot = teta_dot + dt*teta_ddot; %Euler
+    k=k+1;
+    teta_dots_rpm(k) = teta_rpm;
+end
+
+figure(2)
+clf
+hold on 
+title('Start-up time to Steady state')
+ylabel('(d/dt)\phi_{motor} [rpm]')
+xlabel('Time [seconds]')
+plot(tvec, teta_dots_rpm)
+line([0 max(tvec)],[maxPower_motorSpeed maxPower_motorSpeed],'LineStyle',':')
+line([0 max(tvec)],[maxPower_motorSpeed/2 maxPower_motorSpeed/2],'LineStyle',':')
+text(5,maxPower_motorSpeed,'Steady state speed','VerticalAlignment','bottom')
+text(5,maxPower_motorSpeed/2,'Half of steady_state speed','VerticalAlignment','bottom')
+time_half_rpm=interp1(teta_dots_rpm, tvec, maxPower_motorSpeed/2);
+line([time_half_rpm time_half_rpm],[0 maxPower_motorSpeed/2],'LineStyle','-.')
+text(time_half_rpm, maxPower_motorSpeed/4,['\leftarrow Time to reach half of steady state speed = '...
+      num2str(time_half_rpm) 'seconds'],'Fontweight','bold')
