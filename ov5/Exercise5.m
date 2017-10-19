132447789f5e7ab29a7460e90e22ff92dab68494
Odd Harald Sjursen Sande <oddh123@gmail.com>
2017-10-19 13:40:17 +0200

    add function

---
+++ ov5/Exercise5.m
@@ -0,0 +1,12 @@
+%% Problem 1: Finding the tallest point on an island
+
+z = sin(4*pi*x_1)*sin(5*pi*x_2)*(2 - x_1)*(2 - x_2); %%x in km
+z = -z; %% set z to negtive in order to use fmincon
+
+%Quadratic:
+
+
+%Circular: 
+
+
+%% Problem 2: Find the optimal values for R and C.
