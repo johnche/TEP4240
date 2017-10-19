02d922d7e3c1c0540fe85114fccc3b6e64e53962
Simen Norderud jensen <simennj@outlook.com>
2017-10-19 13:14:20 +0200

    tweak constants

--- ov4/one_d.m
+++ ov4/one_d.m
@@ -1,13 +1,18 @@
 clear all
 clc
-R = 100;
-m = 2;
-I = m;
 g = -10;
-c = 10;
-x_0 = [0 ; 0]; % [q_0 ; p_0]
+m = 2;
+k = 2;
+R = 1;
+
+I = m;
+c = 1/k;
+q_0 = 0;
+p_0 = 0;
+
+x_0 = [q_0 ; p_0];
 dx_dt = @(t, x) [x(2)/I ; m*g - x(1)/c - R*x(2)/I];
-%x = [dp_dt ; dq_dt];
-[t, x] = ode45(dx_dt, 1:.1:200, x_0);
+[t, x] = ode45(dx_dt, 1:.01:200, x_0);
+
 plot(t, x)
 legend('p', 'q')
