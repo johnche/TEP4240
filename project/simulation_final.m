aeff6eabaa8bfc49a6761f144ba75a5cd3b6c2b3
Simen <Simennj@outlook.com>
2017-11-14 16:35:41 +0100

    plot force instead of momentum for the speaker

--- project/simulation_final.m
+++ project/simulation_final.m
@@ -1,12 +1,11 @@
 clc
 clear all
 
-wind_speed = 10; % m/s
-rotor_radius = 5; % m
+wind_speed = 8; % m/s
+rotor_radius = 1; % m
 fs = wind_speed * pi*rotor_radius^2;
 
 Tr = .5; % assumption
-
 Gg = .9; % assumption
 
 speaker_radius = .05; %m
@@ -21,7 +20,7 @@ Rr = 10; % assumption
 Rg = 5; % assumption
 Rf = 1;
 Rh = 1; % assumption
-A = @(t) sin(220*pi*t);
+A = @(t) sin(220*2*pi*t);
 
 pm_0 = 0; ph_0 = 0;
 p_0 = [pm_0; ph_0];
@@ -32,12 +31,30 @@ dPh_dt = @(t, p) ((A(t)*dPa_dt*Gh)/Rf-p(2)*Rh/Ih)/(1+C*Gh*Gh*p(2)/Ih);
 
 dp_dt = @(t, p) [ dPm_dt(t, p) ; dPh_dt(t, p) ];
 
-[t, p] = ode45(dp_dt, linspace(0, 10000, 1000), p_0);
+[t, p] = ode15s(dp_dt, linspace(0, 1, 10000), p_0);
 
 %plot(t, p)
 %legend('Pm', 'Ph')
 
-plot(t, p(:,2))
-legend('Linear momentum speaker membrane')
+
+speaker_force = zeros(size(t));
+
+for i = 1:size(t)
+    speaker_force(i) = dPh_dt(t(i), p(i,:));
+end
+
+figure(1)
+plot(t, speaker_force)
+legend('Induced force speaker membrane')
 xlabel('Time (s)')
-ylabel('Linear momentum (N s)')
+ylabel('Force (N)')
+
+figure(2)
+clf
+hold on
+plot(t, speaker_force)
+plot(t, A(t))
+hold off
+ylabel('Force (N)')
+legend('Induced force speaker membrane', 'Mic input')
+xlabel('Time (s)')
